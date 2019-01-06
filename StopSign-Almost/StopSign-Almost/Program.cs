7b7d9ac8deae71700fc4af266e20f6ece662bbc6
SirTomax <46323295+SirTomax@users.noreply.github.com>
2019-01-06 11:27:40 +0100

    Added comments

--- StopSign-Almost/StopSign-Almost/Program.cs
+++ StopSign-Almost/StopSign-Almost/Program.cs
@@ -18,11 +18,11 @@ namespace StopSign_Almost
         // Method to draw an entier row
         static void Draw_Row(char Bef_ch, int Bef, char First_ch, int Between, char Between_ch, char last_ch, bool Elon)
         {
-            Draw_Chars(Bef_ch, Bef);
-            Console.Write(First_ch);
-            Draw_Chars(Between_ch,Between);
-            Console.Write(last_ch);
-            if (Elon == true)
+            Draw_Chars(Bef_ch, Bef); // Draw characters before first
+            Console.Write(First_ch); // Draw starting char 
+            Draw_Chars(Between_ch,Between); // Draw content chars, between first and last
+            Console.Write(last_ch); // Draw last char
+            if (Elon == true) // New line ?
                 Console.Write("\n");
         }
 
@@ -33,11 +33,14 @@ namespace StopSign_Almost
             {
                 int main_iter = 2;
 
+                // Get user input
                 Console.Write("Enter an integer between 1 and 14: ");
                 main_iter = Convert.ToInt32(Console.ReadLine());
 
+                // Draw top of stop sign
                 Draw_Row(' ', main_iter * 2, '+', main_iter * 4 - 1, '-', '+', true);
 
+                // draw row 2 to row n-1 of stop sign (the content)
                 for (int i = 1; i <= main_iter; i++)
                 {
                     Draw_Row(' ', 0, ' ', main_iter * 2 - 1, ' ', '|', false);
@@ -45,6 +48,7 @@ namespace StopSign_Almost
                     Draw_Row(' ', main_iter, '|', main_iter * 2 - 1, ' ', ' ', true);
                 }
 
+                // Draw last row -> Bottom of stop sign
                 Draw_Row(' ', main_iter * 2, '+', main_iter * 4 - 1, '-', '+', true);
             }
         }
